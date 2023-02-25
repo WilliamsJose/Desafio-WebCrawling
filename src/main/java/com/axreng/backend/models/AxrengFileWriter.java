95fa37586b76288a4a1214a4be645036e67648c7
williams <contatowilliamj@gmail.com>
2023-02-25 15:06:43 -0300

    sorting list before saving

--- src/main/java/com/axreng/backend/models/AxrengFileWriter.java
+++ src/main/java/com/axreng/backend/models/AxrengFileWriter.java
@@ -3,12 +3,19 @@ package com.axreng.backend.models;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 public class AxrengFileWriter {
 
-    public void createNewFile(String baseUrl, String keyword, List<String> urlsFound) {
+    public void createNewFile(String baseUrl, String keyword, Set<String> urlsFound) {
         int counter = 0;
+
+        List<String> sortedList = new ArrayList<>(urlsFound);
+        Collections.sort(sortedList, String.CASE_INSENSITIVE_ORDER);
+
         while (true) {
             try {
                 String filename = counter > 0 ? "results_" + keyword + "_(" + counter + ").txt" : "results_" + keyword + ".txt";
@@ -23,7 +30,7 @@ public class AxrengFileWriter {
 
                 newFile.write("Search starting with base URL '" + baseUrl + "' and keyword '" + keyword + "'" + System.lineSeparator());
 
-                urlsFound.forEach(url -> {
+                sortedList.forEach(url -> {
                     try {
                         newFile.write("Result found: " + url + System.lineSeparator());
                     } catch (IOException e) {
@@ -31,7 +38,7 @@ public class AxrengFileWriter {
                     }
                 });
 
-                newFile.write("Search finished with " + urlsFound.size() + " results found");
+                newFile.write("Search finished with " + sortedList.size() + " results found");
 
                 newFile.close();
                 System.out.println("Successfully wrote to the file." + System.lineSeparator());
