1676672a460b768d41b2dd06870042fc66204414
williams <contatowilliamj@gmail.com>
2023-02-25 15:08:23 -0300

    cleaning

--- src/main/java/com/axreng/backend/Main.java
+++ src/main/java/com/axreng/backend/Main.java
@@ -6,9 +6,8 @@ import com.axreng.backend.views.WebCrawlerView;
 public class Main {
     public static void main(String[] args) {
         // TODO validar se tenho ao menos as variáveis BaseUrl e Keyword para iniciar
-        WebCrawler crawler = new WebCrawler("http://hiring.axreng.com/", "four");
+        WebCrawler crawler = new WebCrawler("http://hiring.axreng.com/", "four", 200);
         crawler.addObserver(new WebCrawlerView());
-//        crawler.crawl();
         crawler.init();
     }
 }
